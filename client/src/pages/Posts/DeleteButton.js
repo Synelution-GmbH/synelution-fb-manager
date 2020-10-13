d0c3b2935f619733285e7ad3588d4060d28c84e5
Markus Maelzer <markus.maelzer@gmail.com>
2020-10-13 10:23:39 +0200

    fix color

--- client/src/pages/Posts/DeleteButton.js
+++ client/src/pages/Posts/DeleteButton.js
@@ -7,12 +7,24 @@ import {
   DialogContent,
   DialogTitle,
   IconButton,
+  makeStyles,
 } from '@material-ui/core';
 
 import { AwesomeIcon } from 'ui/components/Icons/Icon';
 
+const useStyles = makeStyles((theme) => ({
+  button: {
+    color: theme.palette.error.main,
+
+    '&:hover': {
+      backgroundColor: 'rgba(244, 67, 54, 0.04)',
+    },
+  },
+}));
 export const DeleteButton = ({ onClick }) => {
   const [open, setOpen] = useState(false);
+
+  const classes = useStyles();
 
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -22,6 +34,7 @@ export const DeleteButton = ({ onClick }) => {
         onClick={handleOpen}
         variant="rounded"
         aria-label="delete"
+        className={classes.button}
         color="secondary"
       >
         <AwesomeIcon icon="trash-alt" />
@@ -40,7 +53,8 @@ export const DeleteButton = ({ onClick }) => {
             startIcon={
               <AwesomeIcon icon="trash-alt" style={{ fontSize: '100%' }} />
             }
-            color="secondary"
+            color="primary"
+            className={classes.button}
             onClick={() => {
               onClick();
               handleClose();
