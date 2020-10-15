27511ac178d1e5b58849786a2ddb66c60983f1f9
Markus Maelzer <markus.maelzer@gmail.com>
2020-10-15 16:47:25 +0200

    add build :c

---
+++ client/build/custom-sw.js
@@ -0,0 +1,18 @@
+console.log('Loaded service worker!');
+console.log('hi');
+
+self.addEventListener('push', (ev) => {
+  const data = ev.data.json();
+  console.log('Got push', data);
+  self.registration.showNotification(data.title, {
+    body: 'Hello, World!',
+    icon: 'http://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png',
+    tag: 'push-notification-tag',
+    data,
+  });
+});
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url));
+});
---
+++ client/build/service-worker.js
@@ -0,0 +1,40 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.3c5067d7b2c498b8f6bc12c9fa4cf165.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
+!function(e){var t={};function n(o){if(t[o])return t[o].exports;var r=t[o]={i:o,l:!1,exports:{}};return e[o].call(r.exports,r,r.exports,n),r.l=!0,r.exports}n.m=e,n.c=t,n.d=function(e,t,o){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:o})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var o=Object.create(null);if(n.r(o),Object.defineProperty(o,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var r in e)n.d(o,r,function(t){return e[t]}.bind(null,r));return o},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){e.exports=n(1)},function(e,t){console.log("Loaded service worker!"),console.log("hi"),self.addEventListener("push",(function(e){var t=e.data.json();console.log("Got push",t),self.registration.showNotification(t.title,{body:"Hello, World!",icon:"http://mongoosejs.com/docs/images/mongoose5_62x30_transparent.png",tag:"push-notification-tag",data:t})})),self.addEventListener("notificationclick",(function(e){e.notification.close(),e.waitUntil(clients.openWindow(e.notification.data.url))}))}]);
