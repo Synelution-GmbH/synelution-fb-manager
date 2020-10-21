7c985cc8d90c7cf7d0efc9db0d612fb61786622b
Markus Maelzer <markus.maelzer@gmail.com>
2020-10-21 10:05:20 +0200

    fix copy image to clipboard

--- client/src/ui/components/Editor/CopyToClipboard.jsx
+++ client/src/ui/components/Editor/CopyToClipboard.jsx
@@ -3,13 +3,52 @@ import { Button } from '@material-ui/core';
 import { AwesomeIcon } from '../Icons/Icon';
 import { askClipboardPermissions } from './helper';
 
+const setToClipboard = (blob) => {
+  const data = [new window.ClipboardItem({ [blob.type]: blob })];
+  return navigator.clipboard.write(data);
+};
+
+const getPngBlob = ({ src }) =>
+  new Promise((resolve, reject) => {
+    const c = document.createElement('canvas');
+    const ctx = c.getContext('2d');
+    const img = new Image();
+    console.log('asd');
+    img.onload = () => {
+      c.height = img.height;
+      c.width = img.width;
+      ctx.drawImage(img, 0, 0);
+      const newName = src.replace(/\.\w{1,}$/g, '.png');
+      console.log(newName);
+      c.toBlob(
+        function (blob) {
+          resolve(blob);
+        },
+        'image/png',
+        1
+      );
+    };
+    img.src = src;
+  });
+
 export const CopyToClipboard = ({ value, ...props }) => {
   const [copied, setCopied] = useState(false);
   const saveToClipboard = async (e) => {
     e.stopPropagation();
-    await askClipboardPermissions();
+    const permission = await askClipboardPermissions();
+    console.log(permission);
     try {
-      await navigator.clipboard.writeText(value);
+      console.log();
+      if (value.search(/\.(gif|jpe?g|bmp)$/g) !== -1) {
+        const blob = await getPngBlob({ src: value });
+        await setToClipboard(blob);
+      } else if (/\.(png)$/g) {
+        const res = await fetch(value);
+        const blob = await res.blob();
+        await setToClipboard(blob);
+      } else {
+        await navigator.clipboard.writeText(value);
+      }
       setCopied(true);
     } catch (error) {
       console.log(error);
--- client/src/ui/components/Editor/helper.js
+++ client/src/ui/components/Editor/helper.js
@@ -2,6 +2,7 @@ export const askClipboardPermissions = async () => {
   try {
     const { state } = await navigator.permissions.query({
       name: 'clipboard-write',
+      allowWithoutGesture: false,
     });
     return state === 'granted';
   } catch (e) {
