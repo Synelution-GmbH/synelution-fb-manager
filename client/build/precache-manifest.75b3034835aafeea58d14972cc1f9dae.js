a01f9da0962cba308eebc57a0fe7d27605f5ea33
Markus Maelzer <markus.maelzer@gmail.com>
2020-12-22 16:01:36 +0100

    build

---
+++ client/build/precache-manifest.75b3034835aafeea58d14972cc1f9dae.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6dcc85d7288979ff66dbf0eb2f180ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a43c933ad23048d8fd93",
+    "url": "/static/css/10.cd7257b5.chunk.css"
+  },
+  {
+    "revision": "85dc3b328bd14f041f03",
+    "url": "/static/css/13.cd7257b5.chunk.css"
+  },
+  {
+    "revision": "df3caac43ad2090a51d3",
+    "url": "/static/css/2.c2bc211b.chunk.css"
+  },
+  {
+    "revision": "4998e784649820442d7f",
+    "url": "/static/css/8.cd7257b5.chunk.css"
+  },
+  {
+    "revision": "a196a40f5e43e4721239",
+    "url": "/static/css/main.554f382f.chunk.css"
+  },
+  {
+    "revision": "7d28c8cfe1abb9eeeeef",
+    "url": "/static/js/0.c0fe9779.chunk.js"
+  },
+  {
+    "revision": "5138240263de7d52b66c",
+    "url": "/static/js/1.fb7d4589.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/static/js/1.fb7d4589.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a43c933ad23048d8fd93",
+    "url": "/static/js/10.fc9f6595.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/static/js/10.fc9f6595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b000ec513ea20e8b0fd9",
+    "url": "/static/js/11.b5f93bf0.chunk.js"
+  },
+  {
+    "revision": "b1c75cd0e222fd58565f",
+    "url": "/static/js/12.e357f011.chunk.js"
+  },
+  {
+    "revision": "85dc3b328bd14f041f03",
+    "url": "/static/js/13.b1ccc3e5.chunk.js"
+  },
+  {
+    "revision": "34d45ce70f94dd2df5e2",
+    "url": "/static/js/14.d703b31c.chunk.js"
+  },
+  {
+    "revision": "bb7e9bd6572fda60baab",
+    "url": "/static/js/15.160b21da.chunk.js"
+  },
+  {
+    "revision": "cc5c01020734e97f94ab",
+    "url": "/static/js/16.5fc58f0c.chunk.js"
+  },
+  {
+    "revision": "926a91a348663a4832ec",
+    "url": "/static/js/17.219eb54f.chunk.js"
+  },
+  {
+    "revision": "df3caac43ad2090a51d3",
+    "url": "/static/js/2.beb3fd58.chunk.js"
+  },
+  {
+    "revision": "730d059198fbaac49030bc2db56e5756",
+    "url": "/static/js/2.beb3fd58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49f7a01db523c046672d",
+    "url": "/static/js/3.a14d9308.chunk.js"
+  },
+  {
+    "revision": "335aba428e2490718a2e",
+    "url": "/static/js/4.b43616f7.chunk.js"
+  },
+  {
+    "revision": "4fef4a2307518fc49161",
+    "url": "/static/js/7.b57ac992.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/7.b57ac992.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4998e784649820442d7f",
+    "url": "/static/js/8.e2c193cd.chunk.js"
+  },
+  {
+    "revision": "34c9fd10d180333843ffa3248b720d4d",
+    "url": "/static/js/8.e2c193cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdef09b3c90eb44d3b9f",
+    "url": "/static/js/9.f33c28e9.chunk.js"
+  },
+  {
+    "revision": "34c9fd10d180333843ffa3248b720d4d",
+    "url": "/static/js/9.f33c28e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a196a40f5e43e4721239",
+    "url": "/static/js/main.503ba84d.chunk.js"
+  },
+  {
+    "revision": "90d4b54cec0c9ad57a62",
+    "url": "/static/js/runtime-main.ac2cda40.js"
+  }
+]);
---
+++ client/build/service-worker.js
@@ -0,0 +1,40 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.75b3034835aafeea58d14972cc1f9dae.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
+!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t,n){e.exports=n(1)},function(e,t,n){"use strict";function r(e,t,n){return t in e?Object.defineProperty(e,t,{value:n,enumerable:!0,configurable:!0,writable:!0}):e[t]=n,e}function o(e,t){var n=Object.keys(e);if(Object.getOwnPropertySymbols){var r=Object.getOwnPropertySymbols(e);t&&(r=r.filter((function(t){return Object.getOwnPropertyDescriptor(e,t).enumerable}))),n.push.apply(n,r)}return n}n.r(t),console.log("Loaded service worker!"),console.log("hi"),self.addEventListener("push",(function(e){var t=e.data.json();console.log("Got push",t),self.registration.showNotification(t.title,function(e){for(var t=1;t<arguments.length;t++){var n=null!=arguments[t]?arguments[t]:{};t%2?o(Object(n),!0).forEach((function(t){r(e,t,n[t])})):Object.getOwnPropertyDescriptors?Object.defineProperties(e,Object.getOwnPropertyDescriptors(n)):o(Object(n)).forEach((function(t){Object.defineProperty(e,t,Object.getOwnPropertyDescriptor(n,t))}))}return e}({icon:"/assets/synelution-icon.png",tag:"fb-tool"},t))})),self.addEventListener("notificationclick",(function(e){e.notification.close(),e.waitUntil(clients.openWindow(e.notification.data.url))}))}]);
